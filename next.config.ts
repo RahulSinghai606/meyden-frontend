0811ab2e8ccda3850168654e730d35c8e28f6a97
rahul.singh <rahul.singh@KELLGGNLPTP1939.local>
2025-11-28 16:33:36 +0530

    Remove static export to support dynamic routes

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
